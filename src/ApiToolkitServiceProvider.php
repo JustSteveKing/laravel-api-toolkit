0dc5d1eeaf122f0a47f639fbf0b5fc211c6f126c
Steve McDougall <juststevemcd@gmail.com>
2020-12-16 10:04:49 +0000

    Starting package skeleton

---
+++ src/ApiToolkitServiceProvider.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace JustSteveKing\Laravel\ApiTookkit;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiToolkitServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the API Toolkit
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // publish stuff
+    }
+}
